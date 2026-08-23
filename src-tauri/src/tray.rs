ad645281088de25c29b9c3017a3c2b5ba6a4b627
Ansel <238927804+asong56@users.noreply.github.com>
2026-08-23 15:52:27 -0400

    Simplify setup function in tray.rs
    Refactor setup function to simplify AppHandle usage and remove unnecessary parameters.

--- src-tauri/src/tray.rs
+++ src-tauri/src/tray.rs
@@ -1,10 +1,10 @@
 use tauri::{
     menu::{Menu, MenuItem},
     tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
-    AppHandle, Manager, Runtime,
+    AppHandle,
 };
 
-pub fn setup<R: Runtime>(app: &AppHandle<R>) -> tauri::Result<()> {
+pub fn setup(app: &AppHandle) -> tauri::Result<()> {
     let show = MenuItem::with_id(app, "show", "Open Wispet", true, None::<&str>)?;
     let sep = tauri::menu::PredefinedMenuItem::separator(app)?;
     let quit = MenuItem::with_id(app, "quit", "Quit Wispet", true, None::<&str>)?;
@@ -23,12 +23,12 @@ pub fn setup<R: Runtime>(app: &AppHandle<R>) -> tauri::Result<()> {
             } = event
             {
                 let app = tray.app_handle();
-                crate::show_query_window(app, "");
+                crate::show_query_window(app);
             }
         })
         .on_menu_event(|app, event| match event.id.as_ref() {
             "show" => {
-                crate::show_query_window(app, "");
+                crate::show_query_window(app);
             }
             "quit" => {
                 app.exit(0);
